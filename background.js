ac25f26cec994a6032dc277736df711c9d434d1d
sh20raj <shaswatraj3@gmail.com>
2024-07-26 22:19:20 +0530

    Multiple Search Engine Options on Popup.html

--- background.js
+++ background.js
@@ -1,35 +1,39 @@
-let engines = ['google', 'bing']; // Default search engines
-let currentEngineIndex = 0;
-
-chrome.storage.sync.get(['engines'], (result) => {
-  if (result.engines) {
-    engines = result.engines;
-  }
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  const currentUrl = new URL(tab.url);
-  const query = currentUrl.searchParams.get('q');
-  let newUrl = '';
-
-  if (currentUrl.hostname.includes('google.com') || currentUrl.hostname.includes('bing.com')) {
-    currentEngineIndex = (currentEngineIndex + 1) % engines.length;
-  } else {
-    currentEngineIndex = 0;
-  }
-
-  const engine = engines[currentEngineIndex];
-  switch (engine) {
-    case 'google':
-      newUrl = `https://www.google.com/search?q=${query}`;
-      break;
-    case 'bing':
-      newUrl = `https://www.bing.com/search?q=${query}`;
-      break;
-    // Add more cases here for other search engines
-  }
-
-  if (newUrl) {
-    chrome.tabs.update(tab.id, { url: newUrl });
-  }
-});
+const engines = {
+    google: 'https://www.google.com/search?q=',
+    bing: 'https://www.bing.com/search?q=',
+    duckduckgo: 'https://duckduckgo.com/?q=',
+    yahoo: 'https://search.yahoo.com/search?p=',
+    baidu: 'https://www.baidu.com/s?wd=',
+    yandex: 'https://yandex.com/search/?text='
+  };
+  
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'toggleEngine') {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0];
+        if (tab && tab.url && tab.id) {
+          const currentUrl = new URL(tab.url);
+          const query = currentUrl.searchParams.get('q');
+          let newUrl = '';
+  
+          if (tab.url.includes('google.com/search')) {
+            newUrl = engines['bing'] + query;
+          } else if (tab.url.includes('bing.com/search')) {
+            newUrl = engines['google'] + query;
+          } else if (tab.url.includes('duckduckgo.com')) {
+            newUrl = engines['yahoo'] + query;
+          } else if (tab.url.includes('yahoo.com/search')) {
+            newUrl = engines['baidu'] + query;
+          } else if (tab.url.includes('baidu.com/s')) {
+            newUrl = engines['yandex'] + query;
+          } else if (tab.url.includes('yandex.com/search')) {
+            newUrl = engines['google'] + query;
+          }
+  
+          if (newUrl) {
+            chrome.tabs.update(tab.id, { url: newUrl });
+          }
+        }
+      });
+    }
+  });
--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
-document.getElementById('toggleButton').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
-      chrome.runtime.sendMessage({ action: 'toggleEngine', url: tab.url });
+document.querySelectorAll('.engine').forEach(item => {
+    item.addEventListener('click', () => {
+      const engine = item.getAttribute('data-engine');
+      chrome.runtime.sendMessage({ action: 'toggleEngine', engine: engine });
     });
   });
-  
